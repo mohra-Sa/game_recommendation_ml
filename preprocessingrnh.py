7f8b71d70074d5e7d2a50ddc3ad2347091421b7b
rnhmohamed <168369206+rnhmohamed@users.noreply.github.com>
2026-04-21 13:26:12 +0200

    Update preprocessingrnh.py

--- preprocessingrnh.py
+++ preprocessingrnh.py
@@ -5,3 +5,8 @@ df = df.drop(columns=['Website'])
 ## fill the nulls with USD
 df['PriceCurrency'] = df['PriceCurrency'].str.strip()
 df['PriceCurrency'] = df['PriceCurrency'].replace('', 'USD')
+
+
+df['HasDRM']  = (df['DRMNotice'].str.strip() != '').astype(int)    # i have 70 non-e epty rows only 
+df['HasExternalAcct'] = (df['ExtUserAcctNotice'].str.strip() != '').astype(int)
+df = df.drop(columns=['LegalNotice', 'DRMNotice', 'ExtUserAcctNotice'])
